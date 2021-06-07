bc8c34ab0bf35d35c5b293281b3355dd8057c13d
Dima <dimserk97@gmail.com>
2021-06-07 22:13:54 +0300

    refactoring

--- script.py
+++ script.py
@@ -1,35 +1,18 @@
-# import os
-# import subprocess
-# import json
+yaml_config_filename = "tests.yml"
+tese_log_filename    = "test_log.txt"
 
-# fl = os.listdir(".")
-
-
-# with open("res.txt", "w") as fout:
-#     res = subprocess.run(["./a.out", "1"], stdout=fout)
-
-# print("./a.out 1:", res.returncode)
-# print("listdir()", fl)
-
-# res = subprocess.run(["diff", "test.c", "script.py"], stdout=subprocess.DEVNULL)
-
-# print("diff:", res.returncode)
-
-# red_underline = "\033[31m\033[4mmessage\033[0m"
-# green_underline = "\033[33m\033[4mmessage\033[0m"
-
-# with open("test_data.json") as jfile:
-#     data = json.load(jfile)
-
-# print(data)
-
-# for item in data:
-#     print(item["file"], item["code"])
+name_key          = "name"
+pre_command_key   = "pre_command"
+command_key       = "command"
+after_command_key = "after_command"
+expected_key      = "expected"
 
 fail_string = "\033[41m\033[37m\033[4m Failed \033[0m"
 pass_string = "\033[42m\033[37m\033[4m Passed \033[0m"
 
+import yaml
 import subprocess
+
 def e_call(command):
     if command == None:
         return None
@@ -38,33 +21,21 @@ def e_call(command):
             shell=True,
             capture_output=True)
 
-import sys
-import yaml
-
-yaml_config_filename = "tests.yml"
-tese_log_filename = "test_log.txt"
-
-pre_command_key = "pre_command"
-command_key = "command"
-after_command_key = "after_command"
-name_key = "name"
-expected_key = "expected"
-
-def _log(stream, result):
+def log(stream, result):
     stream.write("stdout:\n{}\nstderr:\n{}\n".format(
         result.stdout.decode("utf-8") if len(result.stdout) != 0 else "None\n",
         result.stderr.decode("utf-8") if len(result.stderr) != 0 else "None\n"
     ))
 
-def log(stream, p_res, res, a_res, test):
+def log_test(stream, p_res, res, a_res, test):
     if (p_res != None):
         stream.write("Pre command: {}\n".format(test[pre_command_key]))
-        _log(stream, p_res)
+        log(stream, p_res)
         stream.write("\n" + "-" * 40 + "\n\n")
 
 
     stream.write("Command: {}\n".format(test[command_key]))
-    _log(stream, res)
+    log(stream, res)
     stream.write("return:   {}\nexpected: {}\n".format(
         res.returncode,
         test[expected_key]
@@ -73,68 +44,46 @@ def log(stream, p_res, res, a_res, test):
     if (a_res != None):
         stream.write("\n" + "-" * 40 + "\n\n")
         stream.write("After command: {}\n".format(test[after_command_key]))
-        _log(stream, a_res)
+        log(stream, a_res)
 
     stream.write("\n" + "=" * 79 + "\n\n")
 
+def validate_tests(tests):
+    pass
 
 if __name__ == "__main__":
     with open(yaml_config_filename) as yaml_config:
         tests = yaml.load(yaml_config, Loader=yaml.CLoader)
 
+    validate_tests(tests)
+
     total_tests = len(tests)
     print(f"Found {total_tests} tests")
 
-    # TODO Validate tests
-    # TODO print Valid tests
-
     log_file = open("log.txt", "w")
 
-    i = 1
+    i = 0
     for test in tests:
         pre_res = None
         res     = None
         aft_res = None
+        i += 1
 
-        if pre_command_key in  test:
-            pre_res = e_call(test[pre_command_key])
+        pre_res = e_call(test[pre_command_key])
 
         res = e_call(test[command_key])
         test_result = res.returncode
 
-        if after_command_key in test:
-            aft_res = e_call(test[after_command_key])
+        aft_res = e_call(test[after_command_key])
 
-        log(log_file, pre_res, res, aft_res, test)
+        log_test(log_file, pre_res, res, aft_res, test)
 
         # Вывод информации в консоль
-        print("-" * 80)
-        print("{:>3}/{:<3}".format(i, total_tests), end=' ')
-        i += 1
-
-        if test_result == test[expected_key]:
-            print(pass_string, end=' ')
-        else:
-            print(fail_string, end=' ')
-
-        if name_key in test:
-            print(test[name_key])
+        print("-" * 80 + "\n{:>3}/{:<3} {} {}".format(
+            i,
+            total_tests,
+            pass_string if test_result == test[expected_key] else fail_string,
+            test[name_key]
+        ))
 
     log_file.close()
-
-    # log_file = open("log.txt", "w")
-
-    # log_file.write(f"Command: {sys.argv[1]}\n")
-
-    # res = e_call(sys.argv[1])
-    # if res.returncode == 0:
-    #     print("Passed")
-    # else:
-    #     print("Failed")
-
-    # log_file.write(f"stdout: {res.stdout.decode('utf-8')}\n")
-    # log_file.write(f"stderr: {res.stderr.decode('utf-8')}\n")
-
-    # log_file.write(f"command_finished\n")
-
-    # log_file.close()
